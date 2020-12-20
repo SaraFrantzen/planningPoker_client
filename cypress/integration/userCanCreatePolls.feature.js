d101d2e72a042f4747f8482bc218dff93525f66b
sara <saralundkvist86@gmail.com>
2020-12-20 15:42:32 +0100

    creates featuretest for create poll. Adds command and fixture for login

---
+++ cypress/integration/userCanCreatePolls.feature.js
@@ -0,0 +1,37 @@
+describe("User can create poll", () => {
+  beforeEach(() => {
+    cy.server();
+		cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/polls",
+      response: "fixture:polls.json",
+    });
+
+		cy.visit("/");
+		cy.get('[data-cy="createPoll"]').click();
+    cy.login()
+  });
+
+  context("successfully created", () => {
+    beforeEach(() => {
+      cy.route({
+        method: "POST",
+        url: "http://localhost:3000/api/polls",
+        response: { message: "successfully saved" },
+      });
+		});
+		
+    it("user can successfully create polls", () => {
+      cy.get('[data-cy="form-poll"]').within(() => {
+        cy.get('[data-cy="title"]').type("Title");
+        cy.get('[data-cy="description"]').type("description");
+        cy.get('[data-cy="tasks"]').type("tasks");
+      });
+      cy.get('[data-cy="save-poll"]').contains("Save Poll").click();
+      cy.get('[data-cy="save-poll-message"]').should(
+        "contain",
+        "successfully saved"
+      );
+    });
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,1 +1,32 @@
+Cypress.Commands.add("login", () => {
+  cy.server();
+  cy.route({
+    method: "POST",
+    url: "http://localhost:3000/api/auth/*",
+    response: `fixture:successful_login.json`,
+    headers: {
+      uid: `user@epidemic.com`,
+    },
+  });
 
+  cy.route({
+    method: "GET",
+    url: "http://localhost:3000/api/polls",
+    response: "fixture:polls.json",
+  });
+
+  cy.route({
+    method: "GET",
+    url: "http://localhost:3000/api/auth/*",
+    response: `fixture:successful_login.json`,
+    headers: {
+      uid: `user@epidemic.com`,
+    },
+  });
+  cy.visit("/");
+  cy.get("[data-cy='login-form']").within(() => {
+    cy.get("[data-cy='email']").type("user@epidemic.com");
+    cy.get("[data-cy='password']").type("password");
+    cy.get("[data-cy='submit']").click();
+  });
+});
